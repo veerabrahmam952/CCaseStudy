97ab90a04aa64c2bbaec47891bd08ae024e06f1a
Exelsioro <pvlyoyo@gmail.com>
2023-03-08 15:23:57 +0100

    added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { OrdersPageComponent } from './pages/orders-page/orders-page.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { ProductComponent } from './components/common/product/product.component';
+import { OrderComponent } from './components/common/order/order.component';
+import { FakeBackendProvider } from './providers/FakeBackendProvider';
+import { HttpService } from './services/HTTPService';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CartPageComponent,
+    OrdersPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    HttpService,
+    FakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

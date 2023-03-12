c5b6139fb50310182299713d19fca092c74fb000
veerabrahmam952 <veerabrahmam952@gmail.com>
2023-03-12 13:51:18 +0530

    Add Cart Integration and Product Displayintegratio

--- src/app/components/common/product/product.component.ts
+++ src/app/components/common/product/product.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from 'src/app/services/HTTPService';
 
 export interface Tile {
   color: string;
@@ -7,23 +9,80 @@ export interface Tile {
   text: string;
 }
 
+export interface Product {
+  brand: string;
+  category: string;
+  description: string;
+  discountPercentage: number;
+  id: number;
+  images: Array<[]>;
+  price: number;
+  rating: number;
+  stock: number;
+  thumbnail:string;
+  title: string;
+}
+
+export interface productParams{
+  productId: number
+}
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+  
 
   title = 'Card View Demo';
 
   gridColumns = 2;
 
+  product:Product={
+    brand: '',
+    category: '',
+    description: '',
+    discountPercentage: 0,
+    id: 0,
+    images: [],
+    price: 0,
+    rating: 0,
+    stock: 0,
+    thumbnail: '',
+    title: '',
+  };
+
+  productID: number=0;
+
+  constructor(private httpClient: HttpService, private route: ActivatedRoute,){
+    this.route.queryParams.subscribe(async(params) => {
+      if (params && params['productId']) {
+        this.productID = params['productId'];
+        this.getProductById(this.productID);
+      }
+    });
+    
+  }
+
+  ngOnInit() {
+    
+  }
   toggleGridColumns() {
     this.gridColumns = this.gridColumns === 3 ? 4 : 3;
   }
 
   addToCart(num: number){
     alert('Item Added to Cart Successfully');
+  }
+
+  getProductById(prodId:number){
+    let _sub_url_ = "/products/" + prodId;
+    this.httpClient.getData(_sub_url_).subscribe((data:any) => {
+      console.log("Products By Id");
+      console.log(data);
+      this.product = data.product;
+    });
   }
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,33 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Route, Router } from '@angular/router';
+import { HttpService } from 'src/app/services/HTTPService';
+
+export interface Product {
+  brand: string;
+  category: string;
+  description: string;
+  discountPercentage: number;
+  id: number;
+  images: Array<[]>;
+  price: number;
+  rating: number;
+  stock: number;
+  thumbnail: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
-
+export class HomePageComponent implements OnInit {
   title = 'Card View Demo';
 
   gridColumns = 3;
-
-  constructor(private router: Router){
-
+  products: Array<Product> = [];
+  constructor(private router: Router, private httpClient: HttpService) {
+    this.httpClient.getData('/products').subscribe((data: any) => {
+      console.log('/products');
+      console.log(data);
+      this.products = data.products;
+    });
   }
+  ngOnInit(): void {}
   toggleGridColumns() {
     this.gridColumns = this.gridColumns === 3 ? 4 : 3;
   }
 
-  addToCart(num: number){
-    alert('Item Added to Cart Successfully');
+  addToCart(prodId: number) {
+    let _prod_add_obj_ = {
+      user_id: '583c3ac3f38e84297c002546',
+      product_id: prodId,
+    }
+    this.httpClient
+      .postData('/cart/add', _prod_add_obj_)
+      .subscribe((data: any) => {
+        console.log('/cart/add');
+        console.log(data);
+        alert('Item Added to Cart Successfully...!');
+      });
   }
 
-  goToProductDetails(num: number){
+  goToProductDetails(prodId: number) {
     let navigationExtras: NavigationExtras = {
       queryParams: {
-        productId: num
-      }
+        productId: prodId,
+      },
     };
     this.router.navigate(['/product'], navigationExtras);
   }
